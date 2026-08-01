9b2a27fe1d9f303adbd405f47e693245aa7bbf7f
Mike Carter <carter71404@icloud.com>
2026-07-31 20:09:20 -0400

    test is FAILING But decent progress has been made Reading n Write pipeline seems good Testing will be implemented shortly

--- backend/src/module/log/log.service.ts
+++ backend/src/module/log/log.service.ts
@@ -1,22 +1,26 @@
-import { AppError } from "../../errors/app.err.ts";
-import { LogError } from "../../errors/log.err.ts";
 import EventEmitter from "node:events";	
+import { Redis } from "ioredis";
 import { createWriteStream } from "node:fs";
 import path from "node:path";
 import zlib from "node:zlib";
+import { mkdir } from "node:fs/promises";
 import { pipeline } from "node:stream/promises";
 import { Readable } from "node:stream";	
+import { AppError } from "../../errors/app.err.ts";
 import type { LOG } from "../../types/log.d.ts"
-import crypto from "node:crypto";
 
 
-
+type REFACTOR = {
+	id:string;
+	name:string;
+	msg:string;
+};
 
 export class STREAM{
 	private MAX_LENGTH:number = 5_000;
-	private redis:any;
-	constructor(redis:any){
-		this.redis = redis
+	private serverRedis:Redis;
+	constructor(serverRedis:Redis){
+		this.serverRedis = serverRedis;
 	}
 
 		
@@ -26,7 +30,7 @@ export class STREAM{
 	//this function will create a new redis stream 
 	public async init(username:string, logName:string){
 		try{
-			await this.redis.xadd(
+			await this.serverRedis.xadd(
 				`LOG:user:${username}:${logName}`,
 				"*", 
 				"log_name", logName, 
@@ -37,19 +41,40 @@ export class STREAM{
 		}	
 	}
 
+	private refactorData = async(data:[string, any[]]):Promise<{val:REFACTOR[], lastId:string}> => {
+		let val:Array<REFACTOR | any> = [];
+		let lastId:string = "";
+
+			for(const [key, value] of data){
+				if(!Array.isArray(val)) throw new AppError("Data refactoring needs an array.", 400);
+
+				for(let i = 0; i < value.length; i += 2){
+					if(val[i] !== undefined){
+						val.push({ id:key, name:value[i], msg:value[i+1] });				
+						lastId = val[val.length - 1].id;
+					}
+				}
+			};
+		return {
+			val,
+			lastId
+		};
+	}
+
 	//CHECK_MAX_LENGTH
 	//reads the length of the stream's batch using it's id
 	//if length is === 0 stream is pushed null stopping the stream
 	//else the reasults are iterated and pushed to the read stream
-	private CHECK_MAX_LENGTH = async(username:string, logName:string):Promise< null | {startId:string, maxed:boolean} | string> => {
-		let lastId:string = ""; 
+	private CHECK_MAX_LENGTH = async(username:string, logName:string):Promise< null | string | {startId:string, maxed:boolean}> => {
 		let startId:string = "";
 		let maxed:boolean = false;
-		let results:[string, string[]] | null;
+		let results:[string, string[]] | null | any;
+
 		try{
-			const LOG_LENGTH = await this.redis.xlen(`LOG:user:${username}:${logName}`)
+			const LOG_LENGTH = await this.serverRedis.xlen(`LOG:user:${username}:${logName}`)
+
 			if(LOG_LENGTH >= this.MAX_LENGTH){
-				results = await this.redis.xrange(
+				results = await this.serverRedis.xrange(
 					`LOG:user:${username}:${logName}`,
 					"-",
 					"+",
@@ -59,10 +84,11 @@ export class STREAM{
 				if(!results){
 					return null;	
 				}
-				startId = lastId;
-				await this.redis.xtrim(`LOG:user:${username}:${logName}`, 'MINID', lastId);
+				
+				const refactored = await this.refactorData(results);
+				await this.serverRedis.xtrim(`LOG:user:${username}:${logName}`, 'MINID', refactored.lastId);
 
-				return await this.saveToDisk(results, logName)
+				return await this.saveToDisk(refactored.val, logName)
 			}
 			return{ 
 				startId,
@@ -72,82 +98,113 @@ export class STREAM{
 			throw err;
 		}	
 	}
-	 private async *iterate(data:[string, string[]]): AsyncGenerator<string>{
+	 private async *iterate(data:[string, string[]] | any): AsyncGenerator<string>{
 		for(const log of data){
 			yield JSON.stringify(log) + '\n';
 		}
 	} 
 
-	private saveToDisk = async(data:[string, string[]], logName:string):Promise<any> => {
+	private saveToDisk = async(data:[string, string[]] | any, logName:string):Promise<any> => {
+		if(data === null) throw new AppError("Save to disk will not accept null data.", 400);
+		if(!Array.isArray(data)) throw new AppError("Save to disk requires the data to be an array", 400);
+
 		const date = new Date(Date.now()).toLocaleString('en-US', {
 			month: '2-digit',
 			day: '2-digit',
 			year: 'numeric'
 		});
-		const __dirname = import.meta.dirname;
-		const target = path.join(__dirname, logName, `${date}.txt`)
-		const read = Readable.from(this.iterate(data));
-		const write = createWriteStream(target, { encoding: "utf-8" })
-		const gzip = zlib.createGzip();
 		try{
+			const __dirname = import.meta.dirname;
+			const dir:any = await mkdir(path.join(__dirname, logName), { recursive:true })
+			if(dir === undefined) throw new AppError("Save to disk failed. Failed to create dir.", 400); 
+
+			const read = Readable.from(this.iterate(data));
+			const write = createWriteStream(path.join(dir, `${date}.txt.gz`), { encoding: "utf-8" });
+			const gzip = zlib.createGzip();
+
 			await pipeline(read, gzip, write);
+			return `${logName} has reached it's limit, and was successfully saved to disk.` 
 		}catch(err){
 			throw err;
 		}
 	} 
 
-
-	 async write(data:LOG,username:string){
+	public async write(data:LOG, username:string){
 		try{
 			await this.CHECK_MAX_LENGTH(username, data.logName);
-			await this.redis.xadd(
+			await this.serverRedis.xadd(
 				`LOG:user:${username}:${data.logName}`,
 				"*",
 				"log_name",data.logName,
 				"action","LOG"
-			)
+			);
 		}catch(err){
 			throw err;
 		}	
 	 }
 }
 
+const CONNECTION = {
+	connected: "connected",
+	disconnected: "disconnected",
+} as const
+
 export class LoggerService extends EventEmitter{
 	private stream:Map<string, STREAM> = new Map();
-	private redis:any;
+	private clientRedis:Redis;
 	logName:string;
 	logId:string;
 	username:string;
+	private connectionStatus:string = CONNECTION.disconnected;
 
-	constructor({ username, logId, logName }:LOG, redis:any){
+	constructor({ username, logId, logName }:LOG, clientRedis:Redis){
 		super();
 		this.logId = logId;
 		this.logName = logName
 		this.username = username;
-		this.redis = redis;
+		this.clientRedis = clientRedis;
 		this.registerEvents();
 	}
 
 
-	private async registerEvents(){
-		this.on("error", () => {});
-		this.on("connect", async() => {
-			//connect to redis
-			//check redis health
-			//then return connected
-			return "connected";
+	private registerEvents(){
+		this.on("error", (err) => {
+			throw new Error(err);
 		});
+		this.on("connect", () => "CONNECTED");
+		this.on("status", (e:string) => e);
+	}
+
+
+	public async status(){
+		if(this.connectionStatus !== CONNECTION.connected) this.emit("error", "Currently disconnected. Please connect before checking status.");
+		const ping = await this.clientRedis.ping();
+		const status = this.clientRedis.status;
+		if(ping === "PONG" && status === "ready" && this.connectionStatus === CONNECTION.connected){
+			this.emit("status", this.connectionStatus || CONNECTION.connected);
+			return "Look Good!"
+		};
+		this.emit("status", this.connectionStatus || CONNECTION.disconnected);
+		return "Uh-oh something went wrong";
+	}
+
+	public async connect(){
+		const ping = await this.clientRedis.ping();
+		const status = this.clientRedis.status;
+
+		if(ping === "PONG" && status === "ready"){
+			this.emit("connect");
+			return "Okie Dokie";
+		};
+		return "Failed to connect"
 	}
 
 	public async createLog():Promise<void>{
 		if(!this.logId) this.emit("ID wasn't provided failed to make request.");
 		if(!this.logName) this.emit("ID wasn't provided failed to make request.");
 
-		if(!this.logId) throw new AppError("ID wasn't provided failed to make request.", 400);
-		if(!this.logName) throw new AppError("Failed to provide all needed feilds. Please choose a LOG NAME, and the status of the log.",400);
-
 		try{
-			const stream = new STREAM(this.redis);
+			const stream = new STREAM();
 			this.stream.set(this.logId, stream);
 			await stream.init(this.username, this.logName);		
 		}catch(err:any){
@@ -156,10 +213,14 @@ export class LoggerService extends EventEmitter{
 		}
 	}
 
-	public async log(data:{}, logId:string):Promise<void>{
+	public async log(data:LOG):Promise<string>{
 		try{
-			const stream:any = this.stream.get(logId)
+			const res = this.stream.has(this.logId);
+			if(!res) await this.createLog();	
+
+			const stream:any = this.stream.get(this.logId)
 			await stream.write(data)
+			return `Successfully logged message for ${this.logId}` 
 		}catch(err:any){
 			this.emit(err);
 			throw err 
--- backend/src/test/log/happy/save.test.ts
+++ backend/src/test/log/happy/save.test.ts
@@ -2,30 +2,31 @@ import { test, afterEach, describe, it, before, after } from "node:test";
 import { LoggerService, STREAM } from "../../../module/log/log.service.ts";
 import type { LOG } from "../../../types/log.d.ts";
 import assert from "node:assert";
-import Redis from "ioredis";
+import { Redis } from "ioredis";
 
-const redis = new Redis({
-	port:process.env.REDIS_PORT as string,
+const redis = new Redis({ 
+	port:process.env.REDIS_PORT as any,
 	host:process.env.REDIS_HOST,
-	username:process.env.REDIS_USERNAME,
 	password:process.env.REDIS_PASSWORD,
 	retryStrategy: (times:number) =>{
 		const delay = Math.min(times * 10, 2000);
 		return delay;
-	},
-})
+	}
+});
+
 
 const obj:LOG = ({ username:"Joe", logId:"123", logName:"prod1"})
 let log = new LoggerService(obj, redis)
 
 
-before(async() => {
-	await redis.connect({  });
+after(() => {
+	redis.disconnect();
 })
 
-after(async() => {
-	await redis.disconnect();
-})
+test(async() => {
+	const status = await log.connect();		
+	console.log(status);
+});
 
 
 
