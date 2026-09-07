eee025f00731d406e9fdb69d0cbb6d27f005ee05
Mike Carter <carter71404@icloud.com>
2026-09-07 17:47:20 -0400

    stress test

--- api/socket/test/stress.test.ts
+++ api/socket/test/stress.test.ts
@@ -4,6 +4,7 @@ import assert from "node:assert";
 import { build } from "../../app.ts";
 import { log, buildSocket } from "./mocks.ts";
 
+
 /**
  *  TEST WILL INCLUDE: 
  *  Client Socket
@@ -23,33 +24,47 @@ import { log, buildSocket } from "./mocks.ts";
  * Funneling data from server socket to client
  */
 
-
-const app:any = await build()
-await app.ready();
-const req = request(app.server);
-
 let socket:any;
+let app:any;
+let req:any;
+
 before(async() => {
-	socket = await buildSocket();
+	app = await build()
+	try{
+		await app.listen({ port:3000, host:"127.0.0.1"})
+		await app.ready();
+		const url = `http://localhost:${app.server.address().port}`
+		req = request(url);
+		socket = await buildSocket(url);
+	}catch(e){
+		throw e;
+	}
 })
+
 after(async() => {
-	socket.emit("disconnect", () => "Finished Test");
-	socket.close()
+	await new Promise<void>((res) => {
+		socket.once("disconnect", () => res());
+		socket.close();
+	})
 	await app.close();
 });
-test("Send log to client socket", async() => {
-	const res = await req	
-					.post("/api/log")
-					.set("Content-type", "application/json")
-					.send(log);	 
-	assert.strictEqual(res.status, 201);
+
+test("Send log to client socket", { timeout: 800_000, } ,async() => {
+	console.time()
+	const start = process.memoryUsage().heapUsed
+	let res:any;
+	for(let i = 0; i < 1000; i++){
+		res = await req	
+				.post("/api/log")
+				.set("Content-type", "application/json")
+				.send(log);	 
+	}
+	assert.strictEqual(res.ok, true);
+	const end = process.memoryUsage().heapUsed
+	const memory = end - start;
+	console.log(`MEMORY USED MB:`,(memory / 1024 / 1024).toFixed(2));
+	console.timeEnd()
 })
-
-
-
-
-
-
 
 
 
